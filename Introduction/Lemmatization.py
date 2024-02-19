aad33b1724b1888856ce4f102802fb1dc257e0ba
Manu Venugopalan <99719105+nmanuvenugopal@users.noreply.github.com>
2024-02-19 12:11:18 +0000

    Update Lemmatization.py

--- Introduction/Lemmatization.py
+++ Introduction/Lemmatization.py
@@ -1,1 +1,19 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+import spacy
 
+p_stemmer = PorterStemmer()
+nlp = spacy.load('en_core_web_sm')
+
+sentence = "The striped bats are hanging on their feet for best"
+
+doc1 = nlp(sentence)
+for token in doc1:
+    print("Lemmatization")
+    print(f"{token} =======> {token.lemma_}")
+
+print("\n")
+
+for word in sentence.split():
+    print("Stemming")
+    print(f"{word} =======> {p_stemmer.stem(word)}")
